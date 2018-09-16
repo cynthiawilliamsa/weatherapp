7805187c4740b04b04396bf2b5e7849c05d268aa
cynthiawilliamsa <cynthiawilliamsa@gmail.com>
2018-09-15 21:46:21 -0500

    added styles

--- src/components/Titles.js
+++ src/components/Titles.js
@@ -4,8 +4,8 @@ class Titles extends React.Component {
     render() {
         return (
             <div>
-                <h1>Weather finder</h1>
-                <p>Find out the temperature conditions</p>
+                <h1 className="title-container__title">Weather finder</h1>
+                <p className="title-container__subtitle">Find out the temperature conditions</p>
             </div>
         )
     }
